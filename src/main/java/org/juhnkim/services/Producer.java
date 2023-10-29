68393c9f6bb2541d3d8079a38cb7d81bbf80fa61
Juhniel <juhn.sweden@gmail.com>
2023-10-29 23:39:47 +0100

    log4j file and property settings

--- src/main/java/org/juhnkim/services/Producer.java
+++ src/main/java/org/juhnkim/services/Producer.java
@@ -1,12 +1,15 @@
 package org.juhnkim.services;
 
+import org.apache.logging.log4j.Logger;
 import org.juhnkim.models.Message;
+import org.juhnkim.utils.Log;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Random;
 
 public class Producer implements Runnable {
+	private final Logger logger = Log.getInstance().getLogger();
 	private final Buffer buffer;
 	private final Random random = new Random();
 	volatile boolean isRunning = true;
@@ -18,9 +21,12 @@ public class Producer implements Runnable {
 	@Override
 	public void run() {
 		String text = "Random text";
+		int interval = (random.nextInt(10) + 1) * 1000;  // Capture the interval time
+		logger.info("Producer will sleep for {} milliseconds before producing the next message", interval); // Log the interval
+
 		while (isRunning) {
 			try {
-				Thread.sleep((random.nextInt(10) + 1) * 1000);
+				Thread.sleep(interval);
 				buffer.add(new Message(text, LocalDate.now(), LocalTime.now()));
 			} catch (InterruptedException e) {
 				e.printStackTrace();
